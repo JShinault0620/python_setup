d3165eb9828784ddcd76d1697b6b262f717d57e0
JShinault0620 <jshincool@gmail.com>
2022-08-13 14:33:48 -0400

    Added response if lunchbox is initially empty

--- test.py
+++ test.py
@@ -9,13 +9,18 @@ def pack(one, two, three):
 print(pack('python', 'is', 'cool'))
 
 def eat_lunch(food_list):
-	for food in food_list:
-		if food == food_list[0]:
-			print(f'I eat {food}.')
-		elif food == food_list [-1]:
-			print(f'Then I eat {food}.')
-			print('My lunchbox is empty.')
-		else:
-			print(f'Then I eat {food}.')
+	if len(food_list):
+		for food in food_list:
+			if food == food_list[0]:
+				print(f'I eat {food}.')
+			elif food == food_list [-1]:
+				print(f'Then I eat {food}.')
+				print('My lunchbox is empty.')
+			else:
+				print(f'Then I eat {food}.')
+	else:
+		print('My lunchbox is empty.')
 
 eat_lunch(['Apple', 'Burger', 'Sandwich'])
+
+eat_lunch([])
